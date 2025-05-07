0155d31e47e2c14712cd8c5fbc5eba029b075f1f
DAGON Lucas <201062070+Lucas-DAGON@users.noreply.github.com>
2025-05-07 10:22:09 +0200

    Create a debuging window

--- src/Graphical_interface/Starter.py
+++ src/Graphical_interface/Starter.py
@@ -8,12 +8,40 @@
 # Description: This script creates a basic window with a label and a button, for now.     #
 ###########################################################################################
 
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+class Main_Window:
+    def __init__(self, master):
+        self.master = master
+        self.master.config(bg='black')
+        self.master.title("test")
+        self.master.geometry('520x300')
+        self.master.resizable(1,1)
+        self.frame = ttk.Frame(self.master)
+        self.button1 = ttk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
+        self.button1.pack()
+        self.frame.pack()
+
+    def new_window(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = Demo2(self.newWindow)
+
+class Demo2:
+    def __init__(self, master):
+        self.master = master
+        self.frame = ttk.Frame(self.master)
+        self.quitButton = ttk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+        self.quitButton.pack()
+        self.frame.pack()
+        
+    def close_windows(self):
+        self.master.destroy()
+
+def main(): 
+    root = tk.Tk()
+    app = Main_Window(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
