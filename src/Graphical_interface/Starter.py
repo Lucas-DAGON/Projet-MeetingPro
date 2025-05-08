a1c46ee0fe4f31ccb9c36584f0fe3a2b29ec02fe
DAGON Lucas <201062070+Lucas-DAGON@users.noreply.github.com>
2025-05-08 12:03:00 +0200

    test image loading function

--- src/Graphical_interface/Starter.py
+++ src/Graphical_interface/Starter.py
@@ -9,23 +9,51 @@
 ###########################################################################################
 
 import tkinter as tk
+import tkcalendar
+import os
+import sys
+from PIL import Image, ImageTk
 from tkinter import ttk
 
+
+
 class Main_Window:
-    def __init__(self, master):
+    def __init__(self, master, path_to_image:str):
         self.master = master
+        self.master.grid()
         self.master.config(bg='black')
         self.master.title("test")
         self.master.geometry('520x300')
         self.master.resizable(1,1)
         self.frame = ttk.Frame(self.master)
-        self.button1 = ttk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window)
-        self.button1.pack()
+        self.button1 = ttk.Button(self.frame, text = 'New Window', width = 25, command = self.new_window).grid(row=0, column=0, padx=10, pady=10)
+        self.button_calendar = ttk.Button(self.frame, text = 'New Window', width = 10, command = self.open_calendar).grid(row=1, column=0, padx=10, pady=10)
+        
+        self.path_to_image:str = path_to_image
+        self.image = Image.open(path_to_image)
+        self.image = ImageTk.PhotoImage(self.image)
+
+        # Create a label to display the image
+        image_label = tk.Label(self.master, image=self.image).grid(row=0, column=2, padx=10, pady=10)
+        self.image_label.pack()
+
         self.frame.pack()
+
+    def open_calendar(self):
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = Create_Calendar(self.newWindow)
 
     def new_window(self):
         self.newWindow = tk.Toplevel(self.master)
         self.app = Demo2(self.newWindow)
+
+class Create_Calendar:
+    def __init__(self, master):
+        self.master = master
+        self.frame = ttk.Frame(self.master)
+        self.calendar = tkcalendar.Calendar(self.master, selectmode='day', year=2023, month=4, day=25)
+        self.calendar.pack(pady=20)
+        self.frame.pack()
 
 class Demo2:
     def __init__(self, master):
@@ -39,8 +67,11 @@ class Demo2:
         self.master.destroy()
 
 def main(): 
+    os.chdir(sys.path[0])
+    path_to_image = "test.png"
+    full_path:str = os.path.join(os.getcwd(), path_to_image)
     root = tk.Tk()
-    app = Main_Window(root)
+    app = Main_Window(root, full_path)
     root.mainloop()
 
 if __name__ == '__main__':
